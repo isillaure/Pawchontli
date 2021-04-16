009b27dbb167a33edbbcf614288f712dc4401b63
Nayeli Gutiérrez <n.isillaure@gmail.com>
2021-04-16 13:53:25 -0500

    Updated index

--- js/main.js
+++ js/main.js
@@ -65,3 +65,17 @@ $(function () {
         format: 'mm-dd-yyyy'
     });
 });
+
+// Like button
+
+var likeBtn = document.querySelector('.ico');
+
+likeBtn.addEventListener('click', function () {
+    likeBtn.classList.toggle('liked');
+});
+
+document.addEventListener('keydown', function (key) {
+    if (key.key === 'l' || key.key === 'L') {
+        likeBtn.classList.toggle('liked');
+    }
+});
