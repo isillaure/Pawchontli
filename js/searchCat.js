ef2084ff03a2099fb4019fa8ed299b2bed351d83
Nayeli Gutiérrez <n.isillaure@gmail.com>
2021-05-06 01:42:54 -0500

    Updated css in forms

--- js/searchCat.js
+++ js/searchCat.js
@@ -1,47 +1,23 @@
 $(document).ready(async () => {
-<<<<<<< HEAD
-    const pets = await fetch(`http://localhost:8000/api/pets/`, {
-        headers: {
-            //Authorization: `Token ${authtoken}`,
-        },
-    });
-    const arrayPets = await pets.json();
-    console.log(arrayPets);
-    let layoutpets = "";
-    arrayPets.forEach(pet => {
-        if (pet.species == "Cat" || pet.species == "cat") {
-            layoutpets += `
-        <div class="card" style="width: 20rem;">
-                <svg class="ico" viewBox="0 0 24 24" style="width: 3em; height: 3em">
-                    <path
-                        d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z">
-                    </path>
-                </svg>
-                <a href="detailPet.html?id_pet=${pet.id}">
-                    <img class="card-img-top" src="${pet.image}" alt="Pet">
-                </a>s
-                <div class="card-body">
-=======
     const is_adopter = localStorage.getItem("is_adopter")
-    if (is_adopter == "true"){
+    if (is_adopter == "true") {
         const pets = await fetch(`http://localhost:8000/api/pets/`, {
-          headers: {
-            //Authorization: `Token ${authtoken}`,
-          },
+            headers: {
+                //Authorization: `Token ${authtoken}`,
+            },
         });
         const arrayPets = await pets.json();
         console.log(arrayPets);
         let layoutpets = "";
         arrayPets.forEach(pet => {
-            if(pet.species == "Cat" || pet.species == "cat"  ){
-            layoutpets+=`
+            if (pet.species == "Cat" || pet.species == "cat") {
+                layoutpets += `
             <div class="card" style="width: 20rem;">
                     <svg class="ico" viewBox="0 0 24 24" style="width: 3em; height: 3em">
                         <path
                             d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z">
                         </path>
                     </svg>
->>>>>>> eae56cefc4c346dfa03b28b43599c1052e2d16eb
                     <a href="detailPet.html?id_pet=${pet.id}">
                         <img class="card-img-top" src="${pet.image}" alt="Pet">
                     </a>s
@@ -57,8 +33,7 @@ $(document).ready(async () => {
             }
         });
         $('.search-pet').html(layoutpets)
-    }
-    else{
+    } else {
         console.log('No es adoptante');
         window.location.href = "/loginUser.html"
     }
